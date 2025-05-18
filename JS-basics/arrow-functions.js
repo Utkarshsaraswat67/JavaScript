e8b65d80b772be8254df57cd490d20c485fa9962
Utkarsh saraswat <utkarshdev87@gmail.com>
2025-05-18 19:28:40 +0000

    arrow functions

---
+++ JS-basics/arrow-functions.js
@@ -0,0 +1,13 @@
+ const addNumbers = (num1,num2) => {
+    return(num1 + num2);
+ }
+
+ console.log(addNumbers(67,87))
+
+
+
+ 
+ const addNumber = (num1,num2) => {name:"rahul"}
+
+ console.log(addNumber(67,87))
+
